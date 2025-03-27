fb463ef67a48e99b6bbbab60fa49a88e3154410b
Prathamesh Walke <prathameshwalke@macbookpro.mynetworksettings.com>
2025-03-26 21:30:09 -0400

    Solution for Sort colors

--- Sample.java
+++ Sample.java
@@ -6,7 +6,6 @@
 
 // Your code here along with comments explaining your approach
 //Problem 3: Container With Most Water
-//Note: This is problem 3 submission only.
 class Solution {
     public int maxArea(int[] height) {
         //Two pointers can be used here to determine the area we need.
@@ -18,7 +17,7 @@ class Solution {
         int area = 0;
 
         //Loop till we cross left over right indicating that all elements were considered once.
-        while(left < right){
+        while (left < right) {
             //so basically this is the calculation for area of rectangle i.e length * breadth
             int calArea = Math.min(height[left], height[right]) * (right - left);
             // we want to select the max area whenever we find that.
@@ -27,9 +26,9 @@ class Solution {
             //here if the right pointer is of greater height we move the left pointer or else we decrement right
             // In case both the elements are equal we can move either of them
             // I chose to move left for no particular reason
-            if(height[left] <= height[right] ){
+            if (height[left] <= height[right]) {
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
@@ -37,3 +36,50 @@ class Solution {
         return area;
     }
 }
+
+// Time Complexity : O(2N) where traverse the array twice
+// Space Complexity : O(1) overwriting the array
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+// Problem 2: Sort colors
+
+class Solution {
+    public void sortColors(int[] nums) {
+        //Since the array will contain only (0s,1s and 2s) initiating counts as below
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        //first pass to count 0s, 1s and 2s
+        for(int i =0; i< nums.length; i++){
+            if(nums[i] == 0){
+                count0++;
+            }
+            if(nums[i] == 1){
+                count1++;
+            }
+            if(nums[i] == 2){
+                count2++;
+            }
+        }
+        int i = 0 ;
+
+        //Loops to overwrite nums array with 0s,1s and 2s also the second pass
+        while(count0 > 0){
+            nums[i++] = 0;
+            count0--;
+        }
+        while(count1 > 0){
+            nums[i++] = 1;
+            count1--;
+        }
+        while(count2 > 0){
+            nums[i++] = 2;
+            count2--;
+        }
+    }
+}
+
